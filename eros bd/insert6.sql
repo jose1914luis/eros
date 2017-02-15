772b2f30cfa50796ac437453a3fa88f724375c16
josegh <josegh@josegh-Lenovo-B40-80>
2017-02-15 12:53:41 -0500

    Revert "nuevas funcionalidades. - anuncios publicables"
    This reverts commit 201c760c6c6428e66fb06708740db25eb3471898.

--- eros bd/insert6.sql
+++
@@ -1,9 +0,0 @@
-USE `bd_eros`;
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (1, 'Escorts');
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (2, 'Masajes Eroticos');
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (3, 'Travestis');
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (4, 'Gays');
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (5, 'Gigolo');
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (6, 'Web Cam');
-INSERT INTO `bd_eros`.`tipo_anuncio` (`idtipo_anuncio`, `tipo`) VALUES (7, 'Otros');
-
