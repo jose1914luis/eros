83bda283b3748266aa51bbaf48d9152760459678
josegh <josegh@josegh-Lenovo-B40-80>
2017-02-16 16:56:56 -0500

    nuevas funcionalidades. - anuncios publicables

--- pruebas.php
+++ pruebas.php
@@ -1,4 +1,10 @@
-<script src="node_modules/jquery/dist/jquery.js" type="text/javascript"></script>
-<script src="js/prueba.js" type="text/javascript"></script>
-<img id='hola' src="./pag_ima/0ac736bfe1ee195b0421aea0f31f5d4f (1).jpg">
-    
+<?php
+$to      = 'jose1914luis@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: no-responder@paginaerotica.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?> 
