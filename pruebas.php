bfe1b7440dce4616ad3584581317180e088ae493
josegh <josegh@josegh-Lenovo-B40-80>
2017-03-01 17:42:15 -0500

    nuevas funcionalidades. - anuncios publicables

--- pruebas.php
+++ pruebas.php
@@ -1,9 +1,27 @@
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require('./mailin-api-php/V2.0/Mailin.php');
+/*
+ * This will initiate the API with the endpoint and your access key.
+ *
+ */
+$mailin = new Mailin('jose1914luis@gmail.com','06rgqfk3mSAvG7LF');
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+/*
+ * This will send a transactional email
+ *
+ */
+/** Prepare variables for easy use **/ 
 
-// send email
-mail("jose1914luis@gmail.com","My subject",$msg);
+$data = array( "to" => array("jose1914luis@gmail.com"=>"to whom!"),
+			"from" => array("noresponder@paginaerotica.com","from email!"),			
+			"subject" => "My subject",
+			"text" => "This is the text",
+			"html" => "This is the <h1>HTML</h1>",
+			"attachment" => array(),
+			"headers" => array("Content-Type"=> "text/html; charset=iso-8859-1","X-param1"=> "value1", "X-param2"=> "value2","X-Mailin-custom"=>"my custom value", "X-Mailin-IP"=> "102.102.1.2", "X-Mailin-Tag" => "My tag")
+);
+
+var_dump($mailin->send_email($data));
