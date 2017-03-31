061491a147f10dc3d39ba844575e9b0bdaa460c7
josegh <josegh@josegh-Lenovo-B40-80>
2017-03-31 13:18:25 -0500

    nuevas funcionalidades

--- pruebas.php
+++ pruebas.php
@@ -1,25 +1,25 @@
 <?php
 
-include_once 'bd/Anuncio.php';
-include_once 'bd/Correo.php';
-include_once 'bd/Usuario.php';
-
-
-$email = 'jose1914luis@gmail.com';
-$anuncio = new Anuncio();
-$total = $anuncio->total_email($email);
-echo $total;
-//if ($total == 1) {
+//include_once 'bd/Anuncio.php';
+//include_once 'bd/Correo.php';
+//include_once 'bd/Usuario.php';
 //
-//    /* Primera ves que publica un anuncio */
-////    try {
-        $usuario = new Usuario();
-        $correo = new Correo();
+//
+//$email = 'jose1914luis@gmail.com';
+//$anuncio = new Anuncio();
+//$total = $anuncio->total_email($email);
+//echo $total;
+////if ($total == 1) {
 ////
-        $datos = $usuario->getUsuariobyEmail($email);
-        $correo->bienvenida($datos['email'], $datos['contra']);
-        print_r($datos);
-        $correo->enviar();
+////    /* Primera ves que publica un anuncio */
+//////    try {
+//        $usuario = new Usuario();
+//        $correo = new Correo();
+//////
+//        $datos = $usuario->getUsuariobyEmail($email);
+//        $correo->bienvenida($datos['email'], $datos['contra']);
+//        print_r($datos);
+//        $correo->enviar();
 ////    } catch (Exception $ex) {
 ////        echo $ex;
 ////    }
