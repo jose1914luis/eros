a63ef51d024c347b7dc33a44dd2396da0557e4c7
josegh <josegh@josegh-Lenovo-B40-80>
2017-07-07 16:00:57 -0500

    modificaciones nueva version

--- plantillas/head.php
+++ plantillas/head.php
@@ -11,15 +11,15 @@
 <meta name="viewport" content="width=device-width, initial-scale=0.8">
 <meta name="juicyads-site-verification" content="2e12bc1a235350e808f396165b405d66">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link href="/css/general.min.css?v=<?= VERSION ?>" rel="stylesheet" type="text/css"/>
-<!--<link href="/css/general.css?v=<?= time() ?>" rel="stylesheet" type="text/css"/>-->
+<!--<link href="/css/general.min.css?v=<?= VERSION ?>" rel="stylesheet" type="text/css"/>-->
+<link href="/css/general.css?v=<?= time() ?>" rel="stylesheet" type="text/css"/>
 <link href="/css/w3.css?v=<?= VERSION ?>" rel="stylesheet" type="text/css"/>
 <link href="/css/bootstrap-social-gh-pages/bootstrap-social.css" rel="stylesheet" type="text/css"/>
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="/js/google.js" type="text/javascript"></script>
-<!--<script src='https://www.google.com/recaptcha/api.js?hl=es'></script>-->
+<script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
