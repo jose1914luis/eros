74a59fc4665c39e3dd3c1203d0ceb47f748ffbc4
josegh <josegh@josegh-Lenovo-B40-80>
2017-07-10 16:34:30 -0500

    modificaciones nueva version

--- bd/Correo.php
+++ bd/Correo.php
@@ -12,7 +12,13 @@ class Correo {
     private $to;
     private $content;
 
-    public function __construct() {        
+    public function __construct() { 
+        
+        $AKEYfile = fopen("/var/www/eros/bd/AKEY.txt", "r") or die("Unable to open file!");
+        $AKEY = fgets($AKEYfile);
+        $this->apiKey = preg_replace('/\s+/', '', $AKEY);
+        fclose($AKEYfile);
+        
 
         $this->foot = '
 <p>Si tienes algún inconveniente, ponte en contacto con nosotros al correo:  <a href="mailto:admin@paginaerotica.com?Subject=Hola">admin@paginaerotica.com</a></p>
@@ -55,20 +61,20 @@ class Correo {
         $this->subject = 'Anuncio Publicado';
 
 
-        $body = '<h2>Hola, Como estas!!</h2>
+        $body = '<h2>Hola!!</h2>
 <p><b>Gracias por publicar en <a href="http://www.paginaerotica.com/">www.paginaerotica.com</a>.</b> Para nosotros es un placer prestarte nuestros servicios. Estos son los detalles de tu anuncio:</p>
 <p>    
     <b>ID ANUNCIO:</b>' . $idanuncio . '<br>
-    <b>URL:</b> <a href="http://www.paginaerotica.com/P_AN/' . $idanuncio . '/' . $url . '">http://www.paginaerotica.com/P_AN/195/Escorts/Antioquia/</a><br>
+    <b>URL:</b> <a href="http://www.paginaerotica.com/P_AN/' . $idanuncio . $url . '">http://www.paginaerotica.com/P_AN/' . $idanuncio . $url . '</a><br>
     <b>TIPO ANUNCIO:</b> Básico por 30 días con 5 re-publicaciones.</p>
-<p>Publicar en paginaerotica.com no tiene costo alguno, sin embargo puedes hacer que tu anuncio sea más visible y obtener múltiples beneficios:
+<p>En paginaerotica.com puedes hacer que tu anuncio sea más visible y obtener múltiples beneficios:
 <ol>
     <li>Aumenta la visión de tu publicidad.</li>    
     <li>Aumenta el número de clientes potenciales.</li>    
     <li>Tu anuncio permanecerá con nosotros 3 meses.</li>    
     <li>Re-publicaciones ilimitadas.</li>    
 </ol>
-<p>El costo de actualizar tu anuncio es de $10.000 COP.</p>
+<p>El costo de actualizar tu anuncio es de $10.000 COP. <b>una vez haya hecho el pago, responde a este email (<a href="mailto:admin@paginaerotica.com?Subject=Hola">admin@paginaerotica.com</a>)  o via WhatsApp 3197614377 con foto del recibo de consignación y el ID del anuncio. Para hacer efectiva la actualización de tu anuncio</b></p>
 <b style="color: red">FORMAS DE PAGO</b>
 <p>Tienes 3 posibilidades de pago, las tres vinculadas a nuestro  responsable de cobros en Colombia, JOSÉ LUIS GARCÍA HERNÁNDEZ.</p>
 <b style="color: #4c87ff"><ins>POR INGRESO EN VENTANILLA O TRANSFERENCIA DE OTRO BANCO QUE NO SEA BANCOLOMBIA</ins></b>
@@ -137,6 +143,95 @@ class Correo {
     </tr>
 </table>
 <p><b style="color: red">Muy importante!</b> <b>una vez haya hecho el pago responda a este email (<a href="mailto:admin@paginaerotica.com?Subject=Hola">admin@paginaerotica.com</a>)  o via WhatsApp 3197614377 con el recibo de consignación escaneado y el ID del anuncio. </b></p>
+<p>Muchas graciar por usar <a href="http://www.paginaerotica.com/">paginaerotica.com</a> para nosotros en un placer brindarte nuestro servicio</p>
+';
+        $this->content = new SendGrid\Content("text/html", $body . $this->foot);
+    }
+    
+     public function pagoinfo($email) {
+
+        $this->from = new SendGrid\Email(null, "admin@paginaerotica.com");
+        $this->to = new SendGrid\Email(null, $email);
+        $this->subject = 'Actualiza tu Anuncio';
+
+
+        $body = '<h2>Hola!!</h2>
+<p>En paginaerotica.com puedes hacer que tu anuncio sea más visible y obtener múltiples beneficios:
+<ol>
+    <li>Aumenta la visión de tu publicidad.</li>    
+    <li>Aumenta el número de clientes potenciales.</li>    
+    <li>Tu anuncio permanecerá con nosotros 3 meses.</li>    
+    <li>Re-publicaciones ilimitadas.</li>    
+</ol>
+<p>El costo de actualizar tu anuncio es de $10.000 COP. <b>Una vez haya hecho el pago, responde a este email (<a href="mailto:admin@paginaerotica.com?Subject=Hola">admin@paginaerotica.com</a>)  o via WhatsApp 3197614377 con foto del recibo de consignación y el ID del anuncio. Para hacer efectiva la actualización de tu anuncio</b> </p>
+<b style="color: red">FORMAS DE PAGO</b>
+<p>Tienes 3 posibilidades de pago, las tres vinculadas a nuestro  responsable de cobros en Colombia, JOSÉ LUIS GARCÍA HERNÁNDEZ.</p>
+<b style="color: #4c87ff"><ins>POR INGRESO EN VENTANILLA O TRANSFERENCIA DE OTRO BANCO QUE NO SEA BANCOLOMBIA</ins></b>
+<br>
+<br>
+<table>
+    <tr>
+        <th>BANCO:</th>   
+        <td>BANCOLOMBIA</td>
+    </tr>
+    <tr>
+        <th>BENEFICIARIO:</th>
+        <td>JOSÉ LUIS GARCÍA HERNÁNDEZ</td>
+    </tr>
+    <tr>
+        <th>Nº DE CUENTA:</th>
+        <td>31057830151</td>
+    </tr>
+    <tr>
+        <th>IMPORTE NETO:</th>
+        <td>$10.000 Netos  + el valor que cobre la entidad financiera por realizar la transferencia</td>
+    </tr>
+</table>
+<br>
+<b style="color: #4c87ff"><ins>POR TRANSFERENCIA BANCOLOMBIA</ins></b>
+<br>
+<br>
+<table>
+    <tr>
+        <th>BANCO:</th>   
+        <td>BANCOLOMBIA</td>
+    </tr>
+    <tr>
+        <th>BENEFICIARIO:</th>
+        <td>JOSÉ LUIS GARCÍA HERNÁNDEZ</td>
+    </tr>
+    <tr>
+        <th>Nº DE CUENTA:</th>
+        <td>31057830151</td>
+    </tr>
+    <tr>
+        <th>IMPORTE NETO:</th>
+        <td>$10.000 Netos  + el valor que cobre la entidad financiera por realizar la transferencia</td>
+    </tr>
+</table>
+<br>
+<b style="color: #4c87ff"><ins>GIRO POR EFECTY - BALOTO - GANA - SUPERGIROS</ins></b>
+<br>
+<br>
+<table>    
+    <tr>
+        <th>DESTINATARIO:</th>
+        <td>JOSÉ LUIS GARCÍA HERNÁNDEZ</td>
+    </tr>
+    <tr>
+        <th>CÉDULA:</th>
+        <td>1035421260</td>
+    </tr>
+    <tr>
+        <th>CIUDAD:</th>   
+        <td>Medellín</td>
+    </tr>
+    <tr>
+        <th>IMPORTE NETO:</th>
+        <td>$10.000 Netos  + el valor que cobre la entidad financiera por realizar la transferencia</td>
+    </tr>
+</table>
+<p><b style="color: red">Muy importante!</b> <b>una vez haya hecho el pago, responde a este email (<a href="mailto:admin@paginaerotica.com?Subject=Hola">admin@paginaerotica.com</a>)  o via WhatsApp 3197614377 con foto del recibo de consignación y el ID del anuncio. Para hacer efectiva la actualización de tu anuncio</b></p>
 <p>Muchas graciar por usar <a href="http://www.paginaerotica.com/">paginaerotica.com</a> para nosotros en un placer brindarte nuestro servicio</p>
 ';
         $this->content = new SendGrid\Content("text/html", $body . $this->foot);
